3ec5aaa3178124b3fd8bad03cd37aed4b959e9f3
Sergio Rinaudo <kaiohken1982@hotmail.com>
2014-08-01 17:29:08 +0100

    wip

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,17 @@
  */
 angular.module('ngprojectApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+	  $scope.addRow = function () {
+		  $scope.values.push($scope.value);
+		  $scope.value = '';
+		};
+		
+      $scope.orderByField = 'time';
+      $scope.reverseSort = false;
+		
+	  $scope.values = [{
+		  'date': '08/01/2014',
+		  'url': 'www.google.com',
+	      'time': '33'
+	  }];
   });
