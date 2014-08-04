a0e70b030dba9e2a902048fcb4294619b73365ba
Sergio Rinaudo <kaiohken1982@hotmail.com>
2014-08-04 17:30:32 +0100

    wip

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,16 +10,53 @@
 angular.module('ngprojectApp')
   .controller('MainCtrl', function ($scope) {
 	  $scope.addRow = function () {
-		  $scope.values.push($scope.value);
+		  $scope.inputs.push($scope.value);
 		  $scope.value = '';
+		  $scope.updateChart();
 		};
 		
       $scope.orderByField = 'time';
       $scope.reverseSort = false;
-		
-	  $scope.values = [{
-		  'date': '08/01/2014',
-		  'url': 'www.google.com',
-	      'time': '33'
-	  }];
+      
+      $scope.inputs = [{
+    	  'date': '08/01/2014',
+    	  'url': 'www.google.com',
+    	  'time': 33
+      }];
+      
+      $scope.updateMyText = function() {
+          $scope.myText = 'Selected';
+        };
+      
+      $scope.updateChart = function () {		     
+    	  $scope.values = {
+		    		  series: ['Response time'],
+		    		  data: []
+    	  };
+    	  for(var i in $scope.inputs) {
+      
+
+    		  var a = {
+    				  x: $scope.inputs[i].date,
+    				  y: [$scope.inputs[i].time],
+    				  tooltip: $scope.inputs[i].url
+    				  
+    		  };
+    		  $scope.values.data.push(a);
+    		  console.log($scope.values.data);
+    	  }
+      };
+      $scope.updateChart();
+	  
+	  $scope.chartType = 'bar';
+	  
+	  $scope.chartConfig = {
+			  labels: false,
+			  title : 'Response time',
+			  legend : {
+				  display:true,
+				  position:'left'
+			  },
+			  innerRadius: 0
+	  };
   });
