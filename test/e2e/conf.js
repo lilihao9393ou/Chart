6d94614fcd91c43299e7a241fc073602ff17d52a
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2014-08-29 22:59:36 +0200

    e2e test with protractor

---
+++ test/e2e/conf.js
@@ -0,0 +1,12 @@
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec.js'],
+  capabilities: {
+    browserName: 'firefox'
+  },
+//  multiCapabilities: [{
+//    browserName: 'firefox'
+//  }, {
+//    browserName: 'chrome'
+//  }]
+}
